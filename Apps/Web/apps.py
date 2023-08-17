e73f00d6a2a772d8b5b3b1dfdce6a0e98a4fb20b
Faris-ML <71413350+Faris-ML@users.noreply.github.com>
2023-08-17 12:02:23 +0300

    ala-model v2

--- Apps/Web/apps.py
+++ Apps/Web/apps.py
@@ -1,13 +1,11 @@
 from django.apps import AppConfig
 import certifi
 import pymongo
-from transformers import AutoTokenizer
 
 class WebConfig(AppConfig):
     uri = 'mongodb+srv://faris:yxUcNMs5ZiJmTPVE@cluster0.3knsc9a.mongodb.net/?retryWrites=true&w=majority'
     client = pymongo.mongo_client.MongoClient(uri, tlsCAFile=certifi.where())
     # Define DB Name
-    tokenizer = AutoTokenizer.from_pretrained('src/NLP/SSS/tokenizer/')
     db = client['Octopusdb']
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Apps.Web'
